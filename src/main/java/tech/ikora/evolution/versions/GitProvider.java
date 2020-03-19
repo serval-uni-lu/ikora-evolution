d7ceefe04b7cf3d34601f908e91a0debace947b0
kabinja <renaudrwemalika@gmail.com>
2020-03-20 00:21:52 +0100

    clean unused import

--- src/main/java/tech/ikora/evolution/versions/GitProvider.java
+++ src/main/java/tech/ikora/evolution/versions/GitProvider.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.eclipse.jgit.lib.Ref;
 import tech.ikora.BuildConfiguration;
 import tech.ikora.builder.BuildResult;
 import tech.ikora.builder.Builder;
